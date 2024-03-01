79801ac7eaf1c8fceca656395f0122cb85125dd9
Dobberman <pangestubima89@gmail.com>
2024-03-01 17:06:23 +0700

    blogRegistry: add bigio-week-2 blog

--- src/lib/blogRegistry.js
+++ src/lib/blogRegistry.js
@@ -20,5 +20,12 @@ export default [
 		postAt: new Date('2024-02-23'),
 		thumbnail: 'https://github.com/punkestublog/2024-1/blob/main/bigio-week-1/thumb.jpg?raw=true',
 		dir: 'https://github.com/punkestublog/2024-1/blob/main/bigio-week-1/content.md?raw=true'
+	},
+	{
+		slug: 'bigio-week-2',
+		title: 'Mini projek pertama di minggu ke 2',
+		postAt: new Date('2024-03-01'),
+		thumbnail: "https://github.com/punkestublog/2024-1/blob/main/bigio-week-2/thumb.png?raw=true",
+		dir: "https://github.com/punkestublog/2024-1/blob/main/bigio-week-2/content.md?raw=true"
 	}
 ];
